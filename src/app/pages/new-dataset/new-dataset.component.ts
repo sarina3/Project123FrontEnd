91dca1b3369becae304ebb0d6e6ab95a9bbb6afe
SOFTEC\leitnes <Stefan.Leitner@softec.sk>
2020-01-01 20:10:27 +0100

    úprava about + create dataset

--- src/app/pages/new-dataset/new-dataset.component.ts
+++ src/app/pages/new-dataset/new-dataset.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { ImagesService } from 'src/app/services/images.service';
-import { toBase64String } from '@angular/compiler/src/output/source_map';
 
 @Component({
   selector: 'app-new-dataset',
@@ -11,11 +10,12 @@ import { toBase64String } from '@angular/compiler/src/output/source_map';
 export class NewDatasetComponent implements OnInit {
 
   datasetForm = new FormGroup({
-    name: new FormControl(null,Validators.required),
-    metadata: new FormControl(null,Validators.required),
-    images : new FormControl([],Validators.required)
+    name: new FormControl(null, Validators.required),
+    metadata: new FormControl(null, Validators.required),
+    images: new FormControl([], Validators.required)
   });
-  
+  metadataName = null;
+
   constructor(private imageService: ImagesService) { }
 
   ngOnInit() {
@@ -24,35 +24,36 @@ export class NewDatasetComponent implements OnInit {
     );
   }
 
-  onMetadataChange(event){
+  onMetadataChange(event: any) {
     const file = event.target.files[0];
+    this.metadataName = file.name;
     const reader = new FileReader();
     reader.onloadend = () => {
-      this.datasetForm.get('metadata').setValue(<string> reader.result);
-    }
+      this.datasetForm.get('metadata').setValue(reader.result as string);
+    };
     reader.readAsDataURL(file);
   }
 
-  onImagesChange(event){
-    const files:FileList = event.target.files;
-    let tmp = [];
+  onImagesChange(event: any) {
+    const files: FileList = event.target.files;
+    const tmp = [];
     let index = 0;
     const reader = new FileReader();
     reader.onloadend = () => {
-      const name = files.item(index).name
-      tmp.push({name: name, file: <string> reader.result});
+      const name = files.item(index).name;
+      tmp.push({ name: name, file: reader.result as string });
       index++;
-      if(index < files.length){
+      if (index < files.length) {
         reader.readAsDataURL(files.item(index));
-      }else{
+      } else {
         this.datasetForm.get('images').setValue(tmp);
       }
-    }
+    };
     reader.readAsDataURL(files.item(index));
-    
+
   }
 
-  onSubmit(){
+  onSubmit() {
     this.imageService.createDataset(this.datasetForm);
   }
 }
